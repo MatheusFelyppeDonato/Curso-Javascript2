c373d6d9be06d3de1adfbc3a01e250102fdbbc91
Matheus Felippe Donato Escobar <mescobar@e-ig-1720s-iMac.local>
2018-08-15 16:00:35 -0300

    Commit de att do curso

--- js/principal.js
+++ js/principal.js
@@ -8,16 +8,8 @@
 	//console.log(titulo);
 	//console.log(titulo.textContent);
 
-	titulo.textContent = "Aparecida Nutricionista";
-
- 		
- 		var paciente = document.querySelector("#primeiro-paciente");
- 		var tdpeso = paciente.querySelector(".info-peso");
- 		var peso= tdpeso.textContent;
 
 
- 		var tdaltura = paciente.querySelector(".info-altura");
- 		var altura = tdaltura.textContent;
 
  		// console.log(paciente); // tr
  		// console.log(tdpeso); // td que tem o peso
@@ -25,6 +17,57 @@
 
  		//console.log(paciente); // tr
  		 //console.log(tdaltura); // td que tem o peso
- 		 //console.log(altura); // obeter 100
+ 		 //console.log(altura); // obeter 100 
 
- 		 var imc = peso / (altura * altura); // 100 / 2.0 * 2.0 = 100/4 = 25
+	titulo.textContent = "Aparecida Nutricionista";
+
+ 		
+ 		var pacientes = document.querySelectorAll(".paciente");
+
+ 		for (var i = 0; i < pacientes.length; i++) {
+ 			
+ 		var paciente = pacientes[i];
+
+
+
+ 		var tdpeso = paciente.querySelector(".info-peso");
+ 		var peso= tdpeso.textContent;
+
+
+ 		var tdaltura = paciente.querySelector(".info-altura");
+ 		var altura = tdaltura.textContent;
+
+ 		
+
+ 		 var tdImc = paciente.querySelector(".info-imc");
+
+ 		 var pesoEhValido = true;
+ 		 var alturaEhValida = true;
+
+ 		 if ( peso <= 0 || peso >= 1000){
+ 			console.log("Peso Inválido");
+ 			pesoEhValido = false;
+ 			tdImc.textContent = "Peso Inválido"
+ 			paciente.style.backgroundColor = "lightcoral";
+ 		}
+
+
+ 		if ( altura <= 0 || altura >= 3.00) {
+
+ 			console.log("Altura Inválida")
+ 			alturaEhValida = false;
+ 			tdImc.textContent="Altura Inválida" 
+ 		}
+
+
+ 		if ( alturaEhValida && pesoEhValido) {
+ 		var imc = peso / (altura * altura); // 100 / 2.0 * 2.0 = 100/4 = 25
+		
+		tdImc.textContent = imc.toFixed(2);
+
+ 		}
+ 		}
+ 		
+ 		
+
+ 		 
