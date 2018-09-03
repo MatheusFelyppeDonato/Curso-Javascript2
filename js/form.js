914f8118d73786e8a975975a3b485f493933b35d
Matheus Felippe Donato Escobar <mescobar@e-ig-1720s-iMac.local>
2018-09-03 15:54:12 -0300

    commit de validacao de formulario

--- js/form.js
+++ js/form.js
@@ -1,101 +1,130 @@
 /* titulo.addEventListener("click", function(){
- 			console.log("Olha, posso chamar uma função anonima.");
+            console.log("Olha, posso chamar uma função anonima.");
 
- 		});
-
-
- 		function mostraMensagem(){
- 			console.log("Olá, eu fui clicado");
+        });
 
 
- 		} */
+        function mostraMensagem(){
+            console.log("Olá, eu fui clicado");
+
+
+        } */
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
- 		botaoAdicionar.addEventListener("click", function(event) {
- 			event.preventDefault();
+        botaoAdicionar.addEventListener("click", function(event) {
+            event.preventDefault();
 
 
- 			var form = document.querySelector("#form-adiciona");
- 			// extraindo informações do paciente do form
- 				var paciente = obtemPacienteDoFormulario(form);
- 			
+            var form = document.querySelector("#form-adiciona");
+            // extraindo informações do paciente do form
+                var paciente = obtemPacienteDoFormulario(form);
+            
 
- 			// criando a tr do paciente
- 			var pacienteTr = montaTr(paciente);
+            // criando a tr do paciente
+            var pacienteTr = montaTr(paciente);
 
- 			var erro = validaPaciente(paciente);
- 			
+            var erros = validaPaciente(paciente);
+            
 
- 			if(erro.length > 0){
- 				var mensagemErro = document.querySelector("#mensagem-erro");
- 				mensagemErro.textContent = erros;
- 				return;
- 			}
- 			
- 			// adicionando o paciente na tabela
- 			var tabela = document.querySelector("#tabela-pacientes");
+            if(erros.length > 0){
+                exibeMensagensdeErro(erros);
+                return;
+            }
+            
+            // adicionando o paciente na tabela
+            var tabela = document.querySelector("#tabela-pacientes");
 
- 			tabela.appendChild(pacienteTr);
+            tabela.appendChild(pacienteTr);
 
- 			form.reset();
- 		});
+            form.reset();
+            var mensagensErro = document.querySelector("#mensagens-erro");
+            mensagensErro.innerHTML= "";
+        });
 
- 		function obtemPacienteDoFormulario(form) {
-				var paciente = { 
-					nome: form.nome.value,
-					peso: form.peso.value,
-					altura: form.altura.value,
-					gordura: form.gordura.value,
-					imc: calculaimc(form.peso.value, form.altura.value)
- 				}
- 				return paciente;
- 			}
+        function exibeMensagensDeErro(erros){
+            var ul = document.querySelector("#mensagens-erro");
+            ul.innerHTML = "";
+            erros.forEach(function(erro){
+                var li = document.createElement("li");
+                li.textContent = erro;
+                ul.appendChild(li);
 
-
-
- 			function montaTr(paciente){
- 				 var pacienteTr = document.createElement("tr");
- 				 pacienteTr.classList.add("paciente");
+            });
 
 
- 			
- 			pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
- 			pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
- 			pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
- 			pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
- 			pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
- 			
+        }
+
+        function obtemPacienteDoFormulario(form) {
+                var paciente = { 
+                    nome: form.nome.value,
+                    peso: form.peso.value,
+                    altura: form.altura.value,
+                    gordura: form.gordura.value,
+                    imc: calculaimc(form.peso.value, form.altura.value)
+                }
+                return paciente;
+            }
 
 
- 				 return pacienteTr;
+
+            function montaTr(paciente){
+                 var pacienteTr = document.createElement("tr");
+                 
+                    pacienteTr.classList.add("paciente");
+                    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+                    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+                    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+                    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+                    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+            
+
+                 return pacienteTr;
 
 
- 			}
+            }
 
- 			function montaTd(dado, classe){
- 				var td = document.createElement("td");
- 				td.textContent = dado; 
- 				td.classList.add(classe);
- 				return td;
- 			}
+            function montaTd(dado, classe){
+                var td = document.createElement("td");
+                    td.textContent = dado; 
+                    td.classList.add(classe);
+                return td;
+            }
 
- 			function validaPaciente (paciente){
+            function validaPaciente (paciente){
 
- 				var erros = [];
+                var erros = [];
 
- 				if (!validaPeso(paciente.peso))	erros.push("Peso é inválido");
- 				if (!validaAltura(paciente.altura)) erros.push("Altura é inválida");
+                    if ( paciente.nome.length == 0){
+                        erros.push("O nome não pode ser em branco");
 
-	
- 				return erros;
+                    }
 
- 			}
+                    if (!validaPeso(paciente.peso)) erros.push("Peso é inválido");
+                    if (!validaAltura(paciente.altura)) erros.push("Altura é inválida");
 
- 		// titulo.addEventListener("click", function(){ 
- 		//	console.log("fui clicado");
- 	//	});
+                    if ( paciente.gordura.length == 0){
+                        erros.push("A gordura não pode ser em branco");
+                    }
+
+                    if ( paciente.peso.length == 0){
+                        erros.push("O peso não pode ser em branco");
+                    }
+
+                    if (paciente.peso.length == 0){
+
+                        erros.push("A altura não pode ser em branco");
+                    }
+
+    
+                return erros;
+
+            }
+
+        // titulo.addEventListener("click", function(){ 
+        //  console.log("fui clicado");
+    //  });
 
 
- 		
+        
 
- 		 
+         
